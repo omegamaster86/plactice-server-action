7e04f6e7f2e76d4cdf9fc0bd5f4059daf0b75845
omegamaster86 <masya868686@gmail.com>
2025-01-25 22:16:17 +0900

    useActionState追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,27 @@
 "use client";
 
-import { useState, useTransition } from "react";
+import { useActionState } from "react";
 import { getRandomProduct } from "./data";
 
 type Product = { title: string };
 
 export default function Home() {
-	const [product, setProduct] = useState<Product | null>(null);
-
-	const [isPending, startTransition] = useTransition();
-	const handleClick = () => {
-		setProduct(null);
-		startTransition(async () => {
-			const product = await getRandomProduct();
-			setProduct(product);
-		});
-	};
+  const [product, dispatch, isPending] = useActionState(async () => {
+    const product: Product = await getRandomProduct();
+    return product;
+  }, null);
 
 	return (
 		<div className="p-4">
-			<button
-				type="button"
-				onClick={handleClick}
-				disabled={isPending}
-				className="bg-blue-500 px-4 py-2 text-white hover:bg-blue-700 disabled:opacity-50"
-			>
-				商品を取得する
-			</button>
+      <form action={dispatch}>
+        <button
+          type="submit"
+          disabled={isPending}
+          className="bg-blue-500 px-4 py-2 text-white hover:bg-blue-700 disabled:opacity-50"
+        >
+          商品を取得する
+        </button>
+      </form>
 			{product ? (
 				<div className="mt-4 w-80 rounded-lg border bg-white p-6 shadow-lg">
 					<h2 className="mb-4 text-2xl font-bold text-gray-800">
